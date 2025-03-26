2636ab5e99b3a2b055e6d4f7dfacd825274ac0bb
Kumi Gaoo <kumigaoo@gmail.com>
2025-03-26 20:06:58 +0100

    Home done, botones no funcionales

--- biomorph/src/03-Pages/Home/Home.jsx
+++ biomorph/src/03-Pages/Home/Home.jsx
@@ -1,13 +1,29 @@
-import Header from "../../02-Components/Header/Header";
-import Example from "../../02-Components/Example/Example";
-
-const home = () => {
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Header from '../../02-Components/Header/Header';
+import fondo_de_pantalla from '../../../assets/fondo_de_pantalla.png'
+const Home = () => {
     return (
-        <>
-            <Header />
-            <Example />
-        </>
-    )
-}
+      <div className="page-container">
+        <style>
+@import url('https://fonts.googleapis.com/css2?family=Lacquer&display=swap');
+</style>
+        <Header />
+        <div 
+          className="home-page"
+          style={{ 
+            backgroundImage: `url(${fondo_de_pantalla})`,
+          }}
+        >
+          <div className="content">
+            <h1>WHAT'S BIOMORPH?</h1>
+            <p>BIOMORPH IS AN ONLINE SHOP THAT PROVIDES ANY TYPE OF ANIMAL.</p>
+            <button className="cta-button">SEE OUR PRODUCTS</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
 
-export default home;
+export default Home;
--- biomorph/src/main.jsx
+++ biomorph/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client'
+import React from 'react';
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
