6cfe5d439c647c708f73531c609f6f6f1629bbf1
Kumi Gaoo <kumigaoo@gmail.com>
2025-03-31 18:22:49 +0200

    como que laila

--- biomorph/src/02-Components/Footer/Footer.jsx
+++ biomorph/src/02-Components/Footer/Footer.jsx
@@ -1,22 +1,25 @@
-import './Footer.scss';
+import "./Footer.scss";
 
 const Footer = () => {
-
   return (
     <footer className="container-fluid py-3">
       <div className="row footer">
         <div className="footer__row col-12 col-lg-12">
           <h1 className="footer__title">BIOMORPH</h1>
           <div className="footer__nav-item">
-            <a className="footer__link" href="/">Sujeto a cambios. Reservamos derecho de modificación o entrada</a>
+            <a className="footer__link" href="/">
+              Sujeto a cambios. Reservamos derecho de modificación o entrada
+            </a>
           </div>
           <div className="footer__nav-item">
-            <a className="footer__link" href="/">AUTORES: LAILA, ALBERT, ORIOL</a>
+            <a className="footer__link" href="/">
+              AUTORES: LILA, ALBERT, ORIOL
+            </a>
           </div>
         </div>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
