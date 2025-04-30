883a37626d366b724e0eb14bb12b076df0bac6d9
oriolgfarssac <oriolgfarssac@gmail.com>
2025-04-30 19:25:44 +0200

    store WIP

---
+++ biomorph/src/03-Pages/Shop/Shop.jsx
@@ -0,0 +1,51 @@
+
+
+const Shop = () =>{
+
+    const data = [
+        {
+            id: 1,
+            name: 'Dead Duck',
+            price: 100,
+            image: 'https://www.creativefabrica.com/wp-content/uploads/2021/12/20/Dead-Duck-illustration-Design-Vector-eps-Creative-Fabrica.jpg',
+            description: 'A dead duck'
+        },
+        {
+            id: 2,
+            name: 'Dead Fish',
+            price: 45,
+            image: 'https://www.creativefabrica.com/wp-content/uploads/2021/12/20/Dead-Fish-illustration-Design-Vector-eps-Creative-Fabrica.jpg',
+            description: 'A dead fish'
+        },
+        {
+            id: 3,
+            name: 'Dead Bird',
+            price: 50,
+            image: 'https://www.creativefabrica.com/wp-content/uploads/2021/12/20/Dead-Bird-illustration-Design-Vector-eps-Creative-Fabrica.jpg',
+            description: 'A dead bird'
+        },
+        {
+            id: 4,
+            name: 'Dead Cat',
+            price: 60,
+            image: 'https://www.creativefabrica.com/wp-content/uploads/2021/12/20/Dead-Cat-illustration-Design-Vector-eps-Creative-Fabrica.jpg',
+            description: 'A dead cat'
+        },
+        {
+            id: 5,
+            name: 'Dead Dog',
+            price: 70,
+            image: 'https://www.creativefabrica.com/wp-content/uploads/2021/12/20/Dead-Dog-illustration-Design-Vector-eps-Creative-Fabrica.jpg',
+            description: 'A dead dog'
+        }
+    ];
+    
+
+
+    return(
+        <>
+        <h1>Shop</h1>
+        </>
+    )
+    
+}
