9623fa456e4037f9b8d02278914cf6dd310a3bdf
oriolgfarssac <oriolgfarssac@gmail.com>
2025-05-07 20:39:25 +0200

    little progress :-) -_-

--- BIO_Proj/auth/urls.py
+++ BIO_Proj/auth/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import user_info
+from .views import register_user, user_info, change_password
 
 urlpatterns = [
     path('me/', user_info, name='user_info'),
+    path('register/', register_user, name='register'),
+    path('change-password/', change_password, name='change_password'),
 ]
